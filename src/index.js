d95e838b3a916eb4526033ffa4928abf868e5fa4
Miles Marillo <mjm03@hampshire.edu>
2019-06-12 11:02:45 -0400

    lookin a little better

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const beerURL = 'http://localhost:3000/beers'
 // --- helper method to list each beer title from API ---
 function addBeerToList(beer) {
 let beerTopList = document.getElementById('list-group')
-beerTopList.innerHTML += ` <li class="list-group-item" data-id="${beer.id}">${beer.name}</li> `
+beerTopList.innerHTML += ` <li style="font-family:futura;" class="list-group-item" data-id="${beer.id}">${beer.name}</li> `
 }
 
 
@@ -11,9 +11,9 @@ beerTopList.innerHTML += ` <li class="list-group-item" data-id="${beer.id}">${be
 function rollThatBeautifulBeerFootage(beer) {
 let beerShowArea = document.getElementById('beer-detail')
 beerShowArea.innerHTML = `
-<h1> --- === ${beer.name} === --- </h1>
-<img src="${beer.image_url}">
-<h3>${beer.tagline}</h3>
+<h1 style="font-family:futura;text-align:center;">  - = = ${beer.name} = = -  </h1>
+<img style="display:block;margin-left:auto;margin-right:auto;width:10%;" src="${beer.image_url}">
+<h3 style="text-align:center;"><em>${beer.tagline}</em></h3>
 <textarea id="beer-description">${beer.description}</textarea>
 <button id="edit-beer" data-id="${beer.id}" class="btn btn-info">
   Save
@@ -21,15 +21,20 @@ beerShowArea.innerHTML = `
 `
 // --- adds event listener and does the patch ---
 document.getElementById('edit-beer').addEventListener('click', function(e) {
-	fetch(`http://localhost:3000/beers/${e.target.dataset.id}`, {
+	editABeer(e.target)
+	})
+}
+
+function editABeer(target) {
+	fetch(`http://localhost:3000/beers/${target.dataset.id}`, {
   headers: { "Content-Type": "application/json; charset=utf-8" },
   method: 'PATCH',
   body: JSON.stringify({
-    description: e.target.previousElementSibling.value,
+    description: target.previousElementSibling.value,
   })
 })
-     })
 	.then(response => response.json())
+	alert(' 🍺 Beer changes saved! Have a drink to celebrate 🍺 ')
 }
 
 // --- fetches beers from API ---
@@ -49,8 +54,9 @@ beerArea.addEventListener('click', function(e) {
 fetch(`http://localhost:3000/beers/${e.target.dataset.id}`)
 .then(resp => resp.json())
 .then(beerData => {
-	console.log(beerData)
+	// console.log(beerData)
 	rollThatBeautifulBeerFootage(beerData)
 	})
-    console.log(e.target.dataset.id) })
+    // console.log(e.target.dataset.id) 
+	})
 })
